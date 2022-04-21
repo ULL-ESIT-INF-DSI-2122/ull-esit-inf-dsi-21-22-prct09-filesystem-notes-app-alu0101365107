21b5607a534b4be0a4a8eaf1dcd0bfb799b083dd
Hector Rodriguez <alu0101365107@ull.edu.es>
2022-04-21 09:37:53 +0000

    Modificacion 9

---
+++ src/mod9/templateMethod.ts
@@ -0,0 +1,32 @@
+/**
+ * Clase abstracta de TemplateMethod
+ */
+export abstract class TemplateMethod {
+  /**
+   * Clase abstracta de TemplateMethod
+   * @param array Array con los valores
+   */
+  constructor(protected array: number []) {
+  };
+
+  public run() {
+  }
+
+  /**
+   * Método para filtrar los valores de un array según una predicado lógico
+   * @param func Función la cual se encarga de evaluar si el predicado lógico es verdadero
+   * @returns Array con los valores los cuales pasan el predicado lógico
+   */
+  filter(func: (numero: number) => boolean): number[] {
+    const resultado: number[] = [];
+    this.array.forEach((element) => {
+      if (func(element)) {
+        resultado.push(element);
+      }
+    });
+    return resultado;
+  }
+  // protected map(func: (numero: number) => number) {
+
+  // };
+}
