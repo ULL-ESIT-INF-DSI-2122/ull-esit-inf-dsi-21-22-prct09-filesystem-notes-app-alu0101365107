5f76c49018ab56ee1122bc044508033e9c6dbcba
Hector Rodriguez <alu0101365107@ull.edu.es>
2022-04-24 13:33:01 +0000

    Correcion SonarCloud

---
+++ tests/mod9/index.spec.ts
@@ -0,0 +1,16 @@
+// import 'mocha';
+// import {expect} from 'chai';
+// import {InitTemplateMethod} from '../../src/mod9/filterMapAddReduce';
+
+// describe("Clase InitTemplateMethod", () => {
+//   const array: InitTemplateMethod = new InitTemplateMethod([1, 2, 3, 4, 5]);
+//   it("Existe la clase initTemplateMethod", () => {
+//     expect(InitTemplateMethod != undefined).to.be.true;
+//   });
+//   it("El método filter funiona correctamente", () => {
+//     expect(array.filter((x: number) => x > 3)).to.be.eql([4, 5]);
+//   });
+//   it("El método filter funiona correctamente", () => {
+//     expect(array.filter((x: number) => x < 3)).to.be.eql([1, 2]);
+//   });
+// });
